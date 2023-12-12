ea1c6a339c091965b6b053eb99b331facc7c2730
Roshan-Kumar-Mishra <144347812+Roshan-Kumar-Mishra@users.noreply.github.com>
2023-12-12 18:52:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const byteSize = (str) => {
-  // write your code here
-};
+function byteSize(str) {
+  // Create a TextEncoder instance
+  const encoder = new TextEncoder();
 
-// Do not change the code below
-const str = prompt("Enter some string.");
-alert(byteSize(str));
+  // Encode the string to UTF-8 and get the byte length
+  const encoded = encoder.encode(str);
+  const byteLength = encoded.length;
+
+  return byteLength;
+}
+
+// Example usage:
+// const inputString = "Hello, 你好";
+// const sizeInBytes = byteSize(inputString);
+// console.log(`The size of the string in bytes is: ${sizeInBytes}`);
+
